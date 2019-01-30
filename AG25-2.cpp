5b6eb34955b25c7f6cd825caef00c0f37cb7b584
IlyaDavydov2002 <44145869+IlyaDavydov2002@users.noreply.github.com>
2019-01-30 15:58:27 +0300

    Update AG25-2.cpp

--- AG25-2.cpp
+++ AG25-2.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
 using namespace std;
-void rec(int x, int y, int z, int& k) {
-	if (x == y)k++;
+void rec(int a, int b, int c, int& d) {
+	if (a == y)d++;
 	else {
-		if (x <= y && x + 1 != z)rec(x + 1, y, z, k);
-		if (x <= y && x * 2 != z)rec(x * 2, y, z, k);
-		if (x <= y && (x * 2) + 1 != z)rec((x * 2) + 1, y, z, k);
-		if (x <= y && x * 10 != z)rec(10 * x, y, z, k);
+		if (a <= b && a + 1 != c)rec(a + 1, b, c, d);
+		if (a <= b && a * 2 != c)rec(a * 2, b, c, d);
+		if (a <= b && (a * 2) + 1 != c)rec((a * 2) + 1, b, c, d);
+		if (a <= b && a * 10 != c)rec(10 * a, b, c, d);
 	}
 }
 int main()
 {
-	int x, y, z, k = 0;
-	cin >> x >> y >> z;
-	rec(x, y, z, k);
-	cout << k;
+	int a, b, c, d = 0;
+	cin >> a >> b >> c;
+	rec(a, y, c, d);
+	cout << d;
 	return 0;
 }
