972b2efb06828043630c1c781216a3c019246553
IlyaDavydov2002 <44145869+IlyaDavydov2002@users.noreply.github.com>
2019-01-30 15:51:33 +0300

    Update AG25-12.cpp

--- AG25-12.cpp
+++ AG25-12.cpp
@@ -8,13 +8,13 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> A[i];
 	}
-	int ans = A[0] + A[5];
+	int x = A[0] + A[5];
 	for (int i = 0; i < n; i++) {
 		for (int j = i + 5; j < n; j++) {
-			if (ans > A[i] + A[j]) {
-				ans = A[i] + A[j];
+			if (x > A[i] + A[j]) {
+				x = A[i] + A[j];
 			}
 		}
 	}
-	cout << ans;
+	cout << x;
 }
